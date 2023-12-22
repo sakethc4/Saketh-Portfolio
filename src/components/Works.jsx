6002e4d59f0626c36d432284cb83b4dcc96a6a4a
sakethc4 <85804657+sakethc4@users.noreply.github.com>
2023-12-22 13:23:57 -0500

    Update Works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -4,7 +4,6 @@ import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { github } from "../assets";
-import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
@@ -94,4 +93,4 @@ const Works = () => {
   );
 };
 
-export default SectionWrapper(Works, "");
+export default Works;
