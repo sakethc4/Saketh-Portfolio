6ee425861a5002c438be4f1108122791fa41c8b7
sakethc4 <85804657+sakethc4@users.noreply.github.com>
2023-12-22 13:12:02 -0500

    Update Tech.jsx

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { BallCanvas } from "./canvas";
-import { SectionWrapper } from "../hoc";
+import { SectionWrapper } from '../hoc/Index';
 import { technologies } from "../constants";
 
 const Tech = () => {
