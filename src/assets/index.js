b9d466cf8d6dbd0a155c1288922a663044accdae
sakethc4 <saketh.c4@gmail.com>
2024-09-01 17:33:30 -0400

    pathing fix.

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,7 +6,7 @@ import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-import umd from "./umd.png";
+import umd from "./tech/umd.png";
 
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
